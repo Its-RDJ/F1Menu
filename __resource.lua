af9b50ada1994ef18b45d77ba4d80739e948bd5b
Faye <rickdejong0228@gmail.com>
2022-10-01 20:09:01 +0200

    Added Files

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'F1 Menu for ESX made by https://github.com/Its-RDJ'
+
+client_scripts {
+	'client.lua',
+	'@es_extended/locale.lua',
+	'locales/sv.lua',
+	'locales/en.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/sv.lua',
+	'locales/en.lua',
+	'config.lua'
+}
---
+++ locales/en.lua
@@ -0,0 +1,56 @@
+Locales['en'] = {
+	-- Citizen Interactions
+	["citizen_interactions"] 	= "Citizen Interaction",
+	["vehicle_menu"]			= "Vehicle menu",
+	["id_card"]					= "ID Card",
+	["id_card2"]				= "Drivers license",
+	["id_card3"]				= "Weapon license",
+	["blindfold"]				= "Put on/take off the blindfold ",
+	["nobody_near"]				= "No player around",
+	-- ID Menu
+	["check_id"]				= "View your ID",
+	["show_id"]					= "Show your ID",
+	["check_id2"]				= "View your Driver's License",
+	["show_id2"]				= "Show your driver's license",
+	["check_id3"]				= "View your Weapon Permit",
+	["show_id3"]				= "Show your Weapon Permit",
+	["id_menu"]					= "Insert ID",
+	["dl_menu"]					= "Insert Driver's License",
+	["gl_menu"]					= "Insert Weapon Permit",
+	-- Voertuigen Menu
+	["must_in_vehicle"]			= "You must be in a vehicle to access this menu",
+	["engine"]					= "engine",
+	["doors"]					= "Doors",
+	["window"]					= "Windows",
+	["cruise_control"]			= "Cruise Control",
+	["shuff"]					= "Change seat",
+	["turned_off_engine"]		= "Engine off",
+	["turned_on_engine"]		= "Engine on",
+	-- Deuren Menu
+	["hood"]					= "Hood",
+	["trunk"]					= "Trunk",
+	["front_left_door"]			= "Left front door",
+	["front_right_door"]		= "Right front door",
+	["back_left_door"]			= "Left back door",
+	["back_right_door"]			= "Right back door",
+	["closed_hood"]				= "Hood closed!",
+	["opened_hood"]				= "Hood opened!",
+	["closed_door"]				= "Door opend!",
+	["opened_door"]				= "Door closed!",
+	["close_all"]				= "Close all",
+	["closed_all"]				= "All doors closed!",
+	-- Window Menu
+	["window_menu"]				= "Window menu",
+	["front_left_window"]		= "Left front window",
+	["front_right_window"]		= "Right front window",
+	["back_left_window"]		= "Left rear window",
+	["back_right_window"]		= "Right rear window",
+	["opened_window"]			= "Rol your window open",
+	["closed_wondow"]			= "Rol your window shut",
+	-- Cruise Control
+	["disabled"]				= "Switch off",
+	["disabled_cruise_control"]	= "Cruise control disabled",
+	-- Seatbelt
+	["seatbelt_on"]				= "Seat belt fastened!",
+	["seatbelt_off"]			= "seat belt unfastened!"
+}
